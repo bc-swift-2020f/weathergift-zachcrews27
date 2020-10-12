ba5707f81798565581e30d3f3b6a0fe7c862835a
Zach Crews <crewsz@bc.edu>
2020-10-12 13:13:32 -0400

    with .gitignore

---
+++ WeatherGift/APIkeys.swift
@@ -0,0 +1,13 @@
+//
+//  APIkeys.swift
+//  WeatherGift
+//
+//  Created by Zach Crews on 10/12/20.
+//  Copyright © 2020 Zachary Crews. All rights reserved.
+//
+
+import Foundation
+
+struct APIkeys {
+    static let googlePlacesKey = "AIzaSyC7gUhJPhHeCKF5T6Bn4VtDXphyH6IwB_I"
+}
---
+++ WeatherGift/WeatherLocation.swift
@@ -0,0 +1,15 @@
+//
+//  WeatherLocation.swift
+//  WeatherGift
+//
+//  Created by Zach Crews on 10/11/20.
+//  Copyright © 2020 Zachary Crews. All rights reserved.
+//
+
+import Foundation
+
+struct WeatherLocation {
+    var name: String
+    var latitude: Double
+    var longitude: Double
+}
